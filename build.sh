3faf91412d3b82f899e4d0def7db1ec604523651
Le Bao Hiep <baohiep2013@gmail.com>
2022-02-16 22:31:26 +0700

    Bump version to 0.0.8. Update dependencies.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NODE_VERSION=16.13.2
+
 VERSION=$(node -p -e "require('./package.json').version")
 BUILD_CMD="npx pkg index.js --no-bytecode --public-packages \"*\" --public"
 
@@ -9,42 +11,42 @@ npx ncc build ../index.js --minify --out .
 echo "(()=>require(\"text-encoding\"))();" >> index.js # Hack to force inclusion of text-encoding
 
 # Linux amd64
-$BUILD_CMD -t node16.13.0-linux-x64 --out-path linux-amd64
+$BUILD_CMD -t node"$NODE_VERSION"-linux-x64 --out-path linux-amd64
 cd linux-amd64
 mv index scratch-run
 zip "../scratch-run_"$VERSION"_linux_amd64.zip" scratch-run
 cd ..
 
 # Linux arm64
-$BUILD_CMD -t node16.13.0-linux-arm64 --out-path linux-arm64
+$BUILD_CMD -t node"$NODE_VERSION"-linux-arm64 --out-path linux-arm64
 cd linux-arm64
 mv index scratch-run
 zip "../scratch-run_"$VERSION"_linux_arm64.zip" scratch-run
 cd ..
 
 # macOS amd64
-$BUILD_CMD -t node16.13.0-macos-x64 --out-path macos-amd64
+$BUILD_CMD -t node"$NODE_VERSION"-macos-x64 --out-path macos-amd64
 cd macos-amd64
 mv index scratch-run
 zip "../scratch-run_"$VERSION"_macos_amd64.zip" scratch-run
 cd ..
 
 # macOS arm64
-$BUILD_CMD -t node16.13.0-macos-arm64 --out-path macos-arm64
+$BUILD_CMD -t node"$NODE_VERSION"-macos-arm64 --out-path macos-arm64
 cd macos-arm64
 mv index scratch-run
 zip "../scratch-run_"$VERSION"_macos_arm64.zip" scratch-run
 cd ..
 
 # Windows amd64
-$BUILD_CMD -t node16.13.0-win-x64 --out-path win-amd64
+$BUILD_CMD -t node"$NODE_VERSION"-win-x64 --out-path win-amd64
 cd win-amd64
 mv index.exe scratch-run.exe
 zip "../scratch-run_"$VERSION"_win_amd64.zip" scratch-run.exe
 cd ..
 
 # Windows arm64
-$BUILD_CMD -t node16.13.0-win-arm64 --out-path win-arm64
+$BUILD_CMD -t node"$NODE_VERSION"-win-arm64 --out-path win-arm64
 cd win-arm64
 mv index.exe scratch-run.exe
 zip "../scratch-run_"$VERSION"_win_arm64.zip" scratch-run.exe
