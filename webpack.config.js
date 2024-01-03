4b78108a0119a4d9b45558655f8ae988a006f02d
Le Bao Hiep <baohiep2013@gmail.com>
2024-01-03 23:53:11 +0700

    Remove more dead modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const dist = path.resolve(__dirname, 'dist');
 const noop_module_path = path.resolve(__dirname, 'src', 'build', 'noop-module');
+const noop_loader_path = path.resolve(
+  __dirname,
+  'src',
+  'build',
+  'noop-loader.js'
+);
 
 module.exports = {
   mode: 'production',
@@ -13,14 +19,32 @@ module.exports = {
   entry: './src/index.js',
   resolve: {
     alias: {
+      // Replace with fastestsmallesttextencoderdecoder
       'text-encoding$': path.resolve(
         __dirname,
         'src',
         'build',
         'text-encoding'
       ),
-      './tw-iframe-extension-worker': noop_module_path,
-      htmlparser2$: noop_module_path
+
+      // Remove dead modules
+      htmlparser2$: noop_module_path,
+      'canvas-toBlob$': noop_module_path,
+      './tw-iframe-extension-worker$': noop_module_path,
+      './tw-unsandboxed-extension-runner$': noop_module_path,
+      '../blocks/scratch3_core_example$': noop_module_path,
+      '../extensions/scratch3_pen$': noop_module_path,
+      '../extensions/scratch3_wedo2$': noop_module_path,
+      '../extensions/scratch3_music$': noop_module_path,
+      '../extensions/scratch3_microbit$': noop_module_path,
+      '../extensions/scratch3_text2speech$': noop_module_path,
+      '../extensions/scratch3_translate$': noop_module_path,
+      '../extensions/scratch3_video_sensing$': noop_module_path,
+      '../extensions/scratch3_ev3$': noop_module_path,
+      '../extensions/scratch3_makeymakey$': noop_module_path,
+      '../extensions/scratch3_boost$': noop_module_path,
+      '../extensions/scratch3_gdx_for$': noop_module_path,
+      '../extensions/tw$': noop_module_path
     }
   },
   module: {
@@ -28,22 +52,12 @@ module.exports = {
       {
         test: /\.[jt]sx?$/,
         loader: 'esbuild-loader'
-      },
-      {
-        test: /\.(svg|png|mp3)$/i,
-        use: [
-          {
-            loader: path.resolve(__dirname, 'src', 'build', 'noop-loader.js')
-          }
-        ]
       }
     ]
   },
   resolveLoader: {
-    // Replace worker-loader with our own modified version
-    modules: [
-      path.resolve(__dirname, 'src', 'build', 'inline-worker-loader'),
-      'node_modules'
-    ]
+    alias: {
+      'worker-loader': noop_loader_path
+    }
   }
 };
