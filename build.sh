8fac8e8fd40e591f453f616c37d5c39d2f4bc91e
Le Bao Hiep <baohiep2013@gmail.com>
2024-01-09 15:20:19 +0700

    Use Node 20.10.0 and SEA

--- build.sh
+++ build.sh
@@ -1,54 +1,87 @@
 #!/bin/bash
 
-NODE_VERSION=16.16.0
+NODE_VERSION=20.10.0
+NODE_DIST_URL=https://nodejs.org/dist/v"$NODE_VERSION"/node-v"$NODE_VERSION"
 
 VERSION=$(node -p -e "require('./package.json').version")
-BUILD_CMD="npx pkg ../dist/index.js"
 
 rm -rf bin dist
 npx webpack
 
 mkdir bin
 cd bin
+node --experimental-sea-config ../sea-config.json
+
+build_unix() {
+  NODE_ARCH=$1
+  TARGET=$2
+  IS_MACOS=$3
+
+  echo "Building $TARGET"
+
+  URL="$NODE_DIST_URL"-"$NODE_ARCH".tar.xz
+  echo "Downloading $URL"
+  curl "$URL" -o "$NODE_ARCH".tar.xz
+  tar -xf "$NODE_ARCH".tar.xz
+  rm "$NODE_ARCH".tar.xz
+
+  mkdir "$TARGET"
+  mv node-v"$NODE_VERSION"-"$NODE_ARCH"/bin/node "$TARGET"/scratch-run
+  rm -rf node-v"$NODE_VERSION"-"$NODE_ARCH"
+
+  cd "$TARGET"
+  if [ "$IS_MACOS" = true ] ; then
+    npx postject scratch-run NODE_SEA_BLOB ../blob.blob \
+      --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+      --macho-segment-name NODE_SEA
+    ldid -Cadhoc -S scratch-run
+  else
+    npx postject scratch-run NODE_SEA_BLOB ../blob.blob \
+      --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+  fi
+
+  zip "../scratch-run_"$VERSION"_"$TARGET".zip" scratch-run
+  cd ..
+}
+
+build_win() {
+  NODE_ARCH=$1
+  TARGET=$2
+
+  echo "Building $TARGET"
+
+  URL="$NODE_DIST_URL"-"$NODE_ARCH".zip
+  echo "Downloading $URL"
+  curl "$URL" -o "$NODE_ARCH".zip
+  unzip -q "$NODE_ARCH".zip
+  rm "$NODE_ARCH".zip
+
+  mkdir "$TARGET"
+  mv node-v"$NODE_VERSION"-"$NODE_ARCH"/node.exe "$TARGET"/scratch-run.exe
+  rm -rf node-v"$NODE_VERSION"-"$NODE_ARCH"
+
+  cd "$TARGET"
+  npx postject scratch-run.exe NODE_SEA_BLOB ../blob.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+
+  zip "../scratch-run_"$VERSION"_"$TARGET".zip" scratch-run.exe
+  cd ..
+}
 
 # Linux amd64
-$BUILD_CMD -t node"$NODE_VERSION"-linux-x64 --out-path linux-amd64
-cd linux-amd64
-mv index scratch-run
-zip "../scratch-run_"$VERSION"_linux_amd64.zip" scratch-run
-cd ..
+build_unix "linux-x64" "linux-amd64" false
 
 # Linux arm64
-$BUILD_CMD -t node"$NODE_VERSION"-linux-arm64 --out-path linux-arm64
-cd linux-arm64
-mv index scratch-run
-zip "../scratch-run_"$VERSION"_linux_arm64.zip" scratch-run
-cd ..
+build_unix "linux-arm64" "linux-arm64" false
 
 # macOS amd64
-$BUILD_CMD -t node"$NODE_VERSION"-macos-x64 --out-path macos-amd64
-cd macos-amd64
-mv index scratch-run
-zip "../scratch-run_"$VERSION"_macos_amd64.zip" scratch-run
-cd ..
+build_unix "darwin-x64" "macos-amd64" true
 
 # macOS arm64
-$BUILD_CMD -t node"$NODE_VERSION"-macos-arm64 --out-path macos-arm64
-cd macos-arm64
-mv index scratch-run
-zip "../scratch-run_"$VERSION"_macos_arm64.zip" scratch-run
-cd ..
+build_unix "darwin-arm64" "macos-arm64" true
 
 # Windows amd64
-$BUILD_CMD -t node"$NODE_VERSION"-win-x64 --out-path win-amd64
-cd win-amd64
-mv index.exe scratch-run.exe
-zip "../scratch-run_"$VERSION"_win_amd64.zip" scratch-run.exe
-cd ..
+build_win "win-x64" "win-amd64"
 
 # Windows arm64
-$BUILD_CMD -t node"$NODE_VERSION"-win-arm64 --out-path win-arm64
-cd win-arm64
-mv index.exe scratch-run.exe
-zip "../scratch-run_"$VERSION"_win_arm64.zip" scratch-run.exe
-cd ..
+build_win "win-arm64" "win-arm64"
